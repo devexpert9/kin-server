e3d22afe4b26c0d2a5cb4f46d30fded9163609fa
root <manmohitindiit@gmail.com>
2020-06-30 13:57:17 +0530

    server up

--- api/routes/webRoutes.js
+++ api/routes/webRoutes.js
@@ -7,19 +7,19 @@ var adduser = require('../controllers/userCtrl');
      .post(adduser.registerUser);
 
 //----- Profile Page Callings-------------------------------
-var profiles = require('../controllers/userCtrl');
+var profiles = require('../controllers/profilesCtrl');
    app.route('/addProfile')
      .post(profiles.addProfile);
 
-var profiles = require('../controllers/userCtrl');
+var profiles = require('../controllers/profilesCtrl');
    app.route('/updateProfile')
      .post(profiles.updateProfile);
 
-var profiles = require('../controllers/userCtrl');
+var profiles = require('../controllers/profilesCtrl');
    app.route('/getProfiles')
      .post(profiles.getProfiles);
 
-var profiles = require('../controllers/userCtrl');
+var profiles = require('../controllers/profilesCtrl');
    app.route('/deleteProfile')
      .post(profiles.deleteProfile);
 
