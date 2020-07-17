cbeceb928020434db28c32287cf613febe0ee604
root <manmohitindiit@gmail.com>
2020-07-17 19:31:33 +0530

    server up

--- socket.js
+++ socket.js
@@ -7,9 +7,22 @@
   port = process.env.PORT || 3001;
   // app.listen(port);
   module.exports = app;
-  io.on('chat_message', (data) => {
-     console.log('a user connected');
-    io.emit('chat_message', {text: data.message, created: new Date()});    
-  });
+  // io.on('chat_message', (data) => {
+  //    console.log('a user connected');
+  //   io.emit('chat_message', {text: data.message, created: new Date()});    
+  // });
+
+  io.on('connection', function (socket) {
+    console.log('ping-pong started');
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+
+    socket.on('new-message', function (data) {
+        console.log(data);
+        socket.emit('message', data);
+    });
+});
   console.log('todo list RESTful API server started on: ' + port);
  
