0249073c02b956973fe0f25f596ce1b53223a500
root <manmohitindiit@gmail.com>
2020-07-17 19:06:32 +0530

    server up

--- socket.js
+++ socket.js
@@ -1,89 +1,13 @@
-  // var express = require('express'),
-  // app = express();
-  // var http = require('http').Server(app);
-  // var io = require('socket.io')(http),
-  // port = process.env.PORT || 3001;
-  // app.listen(port);
-  // module.exports = app;
-  // io.on('chat_message', (data) => {
-  //    console.log('a user connected');
-  //   io.emit('message', {text: data.message, created: new Date()});    
-  // });
-  // console.log('todo list RESTful API server started on: ' + port);
- // Setup basic express server
-var express = require('express');
-var app = express();
-var path = require('path');
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-var port = process.env.PORT || 3001;
-
-server.listen(port, () => {
-  console.log('Server listening at port %d', port);
-});
-
-// Routing
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// Chatroom
-
-var numUsers = 0;
-
-io.on('connection', (socket) => {
-  var addedUser = false;
-
-  // when the client emits 'new message', this listens and executes
-  socket.on('new message', (data) => {
-    console.log('here')
-    // we tell the client to execute 'new message'
-    socket.broadcast.emit('new message', {
-      username: socket.username,
-      message: data
-    });
+  var express = require('express'),
+  app = express();
+  var http = require('http').Server(app);
+  var io = require('socket.io')(http),
+  port = process.env.PORT || 3001;
+  app.listen(port);
+  module.exports = app;
+  io.on('chat_message', (data) => {
+     console.log('a user connected');
+    io.emit('chat_message', {text: data.message, created: new Date()});    
   });
-
-  // when the client emits 'add user', this listens and executes
-  socket.on('add user', (username) => {
-    if (addedUser) return;
-
-    // we store the username in the socket session for this client
-    socket.username = username;
-    ++numUsers;
-    addedUser = true;
-    socket.emit('login', {
-      numUsers: numUsers
-    });
-    // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('user joined', {
-      username: socket.username,
-      numUsers: numUsers
-    });
-  });
-
-  // when the client emits 'typing', we broadcast it to others
-  socket.on('typing', () => {
-    socket.broadcast.emit('typing', {
-      username: socket.username
-    });
-  });
-
-  // when the client emits 'stop typing', we broadcast it to others
-  socket.on('stop typing', () => {
-    socket.broadcast.emit('stop typing', {
-      username: socket.username
-    });
-  });
-
-  // when the user disconnects.. perform this
-  socket.on('disconnect', () => {
-    if (addedUser) {
-      --numUsers;
-
-      // echo globally that this client has left
-      socket.broadcast.emit('user left', {
-        username: socket.username,
-        numUsers: numUsers
-      });
-    }
-  });
-});
+  console.log('todo list RESTful API server started on: ' + port);
+ 
