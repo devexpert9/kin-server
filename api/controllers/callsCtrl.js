19ce2c93fd9ede570a3d2213b810d1011f0cd5d4
root <manmohitindiit@gmail.com>
2020-09-17 16:38:37 +0530

    sdsadsad

--- api/controllers/callsCtrl.js
+++ api/controllers/callsCtrl.js
@@ -86,38 +86,49 @@ exports.getProfileCalls = function(req, res)
 {
     calls.find({userId: req.body.userId, profileId: req.body.profileId }, function(err, all_calls)
     {
-      var counter = 0,
+      if(all_calls)
+      {
+        var counter = 0,
           data = [],
           dict = {};
 
-      function getUserDetails(){
-        if(counter < all_calls.length)
+        function getUserDetails()
         {
-          contacts.findOne({_id: all_calls[counter].contactId}, function(err, doc)
+          if(counter < all_calls.length)
           {
-            dict = {
-              id: all_calls[counter]._id,
-              contactId: all_calls[counter].contactId,
-              contactName: doc.name,
-              callDate: all_calls[counter].callDate,
-              callTime: all_calls[counter].callTime,
-              userId: all_calls[counter].userId,
-              profileId: all_calls[counter].profileId,
-              created_on: all_calls[counter].created_on
-            };
-            data.push(dict);
-            counter = counter + 1;
-            getUserDetails();
-          });
-        }else{
-          res.json({
-             status: 1,
-             data: data,
-             error:null
-          });
-        }
-      };
-      getUserDetails();
+            contacts.findOne({_id: all_calls[counter].contactId}, function(err, doc)
+            {
+              dict = {
+                id: all_calls[counter]._id,
+                contactId: all_calls[counter].contactId,
+                contactName: doc.name,
+                callDate: all_calls[counter].callDate,
+                callTime: all_calls[counter].callTime,
+                userId: all_calls[counter].userId,
+                profileId: all_calls[counter].profileId,
+                created_on: all_calls[counter].created_on
+              };
+              data.push(dict);
+              counter = counter + 1;
+              getUserDetails();
+            });
+          }else{
+            res.json({
+               status: 1,
+               data: data,
+               error:null
+            });
+          }
+        };
+        getUserDetails();
+      }
+      else{
+        res.json({
+               status: 1,
+               data: data,
+               error:null
+            });
+      }
     });
 };
 
