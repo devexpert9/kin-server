654f0533d379cf9ad7cefb4a47ceadb2a189326d
root <manmohitindiit@gmail.com>
2021-03-09 16:29:40 +0530

    sd

--- api/controllers/chatCtrl.js
+++ api/controllers/chatCtrl.js
@@ -155,13 +155,32 @@ exports.save_message = function(req, res){
     });
 };
 
-exports.list_messages = function(req, res){
-  chats.find({'chatId': req.body.chatId}, function(err, doc){
-    patient.findOne({'_id': doc[0].senderId}, function(err, sender){
-      if(sender == null){
-        contacts.findOne({'_id': doc[0].senderId}, function(err, sender){
-          contacts.findOne({'_id': doc[0].receiverId}, function(err, receiver){
-            if(receiver == null){
+exports.list_messages = function(req, res)
+{
+  chats.find({'chatId': req.body.chatId}, function(err, doc)
+  {
+    if(doc != null)
+    {
+      patient.findOne({'_id': doc[0].senderId}, function(err, sender)
+      {
+        if(sender == null)
+        {
+          contacts.findOne({'_id': doc[0].senderId}, function(err, sender){
+            contacts.findOne({'_id': doc[0].receiverId}, function(err, receiver){
+              if(receiver == null){
+                patient.findOne({'_id': doc[0].receiverId}, function(err, receiver){
+                  res.send({
+                    data: {'chat': doc, 'sender': sender, 'receiver': receiver},
+                    status: 1,
+                    error: null
+                  });
+                });
+              }
+            });
+          });
+        }else{
+          contacts.findOne({'_id': doc[0].senderId}, function(err, sender){
+            // if(sender == null){
               patient.findOne({'_id': doc[0].receiverId}, function(err, receiver){
                 res.send({
                   data: {'chat': doc, 'sender': sender, 'receiver': receiver},
@@ -169,23 +188,18 @@ exports.list_messages = function(req, res){
                   error: null
                 });
               });
-            }
-          });
-        });
-      }else{
-        contacts.findOne({'_id': doc[0].senderId}, function(err, sender){
-          // if(sender == null){
-            patient.findOne({'_id': doc[0].receiverId}, function(err, receiver){
-              res.send({
-                data: {'chat': doc, 'sender': sender, 'receiver': receiver},
-                status: 1,
-                error: null
-              });
-            });
 
-        });
-      }
-    });
+          });
+        }
+      });
+    }
+    else{
+      res.send({
+        data: null,
+        status: 0,
+        error: "No any message found"
+      });
+    }
   });
 };
 
